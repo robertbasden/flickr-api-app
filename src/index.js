7d55bdc61db2cb5fc14c694a74e80f2c1da7c73f
Robert Basden <rbasden@nvidia.com>
2017-12-12 21:22:28 +0000

    Initial version of selecting user

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
 import { Provider, connect } from 'react-redux';
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
@@ -15,28 +15,52 @@ const defaultState = {
     error: false
 };
 
-const flickrReducer = (state = defaultState, action) => {
+const photosReducer = (state = defaultState, action) => {
     switch(action.type) {
         case 'FETCH_STARTED':
-            return { photos: [], fetching: true, error: true }
+            return { data: [], fetching: true, error: true }
         case 'DATA_FETCHED':
-            return { photos: action.payload, fetching: false, error: false };
+            return { data: action.payload, fetching: false, error: false };
             break;
         case 'DATA_ERROR':
-            return { photos: [], fetching: false, error: true }
+            return { data: [], fetching: false, error: true }
         default:
             return state;
             break;
     }
 }
 
+const defaultSelectedUserState = {
+    user: null
+};
+
+const selectedUserReducer = (state = defaultSelectedUserState, action) => {
+    switch(action.type) {
+        case 'FETCH_USER_CANCELLED':
+            return { user: null, fetching: false, error: false };
+        case 'FETCH_USER_STARTED':
+            return { user: null, fetching: true, error: false };
+        case 'FETCH_USER_COMPLETE':
+            return { user: action.payload, fetching: false, error: false };
+            break;
+        case 'FETCH_USER_ERROR':
+            return { user: null, fetching: false, error: true };
+        default:
+            return state;
+            break;
+    }
+}
+
+
+
 const middleware = applyMiddleware(thunk, logger);
-const store = createStore(flickrReducer, middleware);
+const combinedReducer = combineReducers({ photos: photosReducer, selectedUser: selectedUserReducer });
+const store = createStore(combinedReducer, middleware);
 
 store.dispatch(dispatch => {
     dispatch({ type: 'FETCH_STARTED' });
     jsonp("https://api.flickr.com/services/feeds/photos_public.gne?format=json", {
-        name: "jsonFlickrFeed"
+        param: "jsoncallback"
     }, function(err, data) {
         if(err != null) {
             dispatch({ type: 'DATA_ERROR' });
@@ -46,9 +70,44 @@ store.dispatch(dispatch => {
     });
 });
 
+const selectUser = user_id => {
+    store.dispatch(dispatch => {
+        dispatch({ type: 'FETCH_USER_STARTED' });
+        jsonp(`https://api.flickr.com/services/rest/?method=flickr.people.getInfo&api_key=636233aa890a436890fce8798558aae9&user_id=${user_id}&format=json`, {
+            param: "jsoncallback"
+        }, function(err, data) {
+            if(err != null) {
+                dispatch({ type: 'FETCH_USER_ERROR' });
+            } else {
+                dispatch({ type: 'FETCH_USER_COMPLETE', payload: data.person });
+            }
+        });
+    });
+}
 
+const cancelSelectUser = () => {
+    store.dispatch({ type: 'FETCH_USER_CANCELLED' });
+}
+
+class UserDetails extends Component {
+    render() {
+        return (
+            <div>
+                <img src={this.props.thumbNail} />
+                <div>{this.props.username}</div>
+                <div>{this.props.realname}</div>
+                <div>{this.props.location}</div>
+                <div>{this.props.description}</div>
+            </div>
+        );
+    }
+}
 
 class Tile extends Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
     backgroundStyle(image) {
         const style = { backgroundImage: "url(" + image + ")" };
         return style;
@@ -60,9 +119,13 @@ class Tile extends Component {
             return "Untitled";
         }
     }
+    handleClick(e) {
+        e.preventDefault();
+        selectUser(this.props.author_id);
+    }
     author(author, author_id) {
         const authorName = author.match( /"(.*?)"/ )[1];
-        return (<a href={'https://www.flickr.com/photos/' + author_id} target="_blank">{authorName}</a>);
+        return (<a href={'https://www.flickr.com/photos/' + author_id} onClick={this.handleClick} target="_blank">{authorName}</a>);
     }
     tags(tags) {
         if(tags.length > 0) {
@@ -110,12 +173,12 @@ class PhotoTiles extends Component {
 
 const mapStateToProps = state => {
     return {
-        photos: state.photos
+        photos: state.photos.data
     }
 }
 
 const mapDispatchToProps = dispatch => {
-    return {}
+    return {};
 }
 
 const ConnectedPhotoTiles = connect(
@@ -124,6 +187,29 @@ const ConnectedPhotoTiles = connect(
 )(PhotoTiles)
 
 class App extends Component {
+    cancel() {
+        cancelSelectUser();
+    }
+    getThumbnailUrl() {
+        return `https://farm1.staticflickr.com/${this.props.selectedUser.iconserver}/buddyicons/${this.props.selectedUser.id}.jpg?1200501238#12037949754@N01`;
+    }
+    getSelectedUserPopup() {
+        if(this.props.selectedUser == null) {
+            return (<div></div>)
+        } else {
+            return (<div>
+                <div className="user-details-blocker" onClick={this.cancel}></div>
+                <div className="user-details">
+                    <UserDetails
+                        thumbNail={this.getThumbnailUrl()}
+                        username={this.props.selectedUser.username._content}
+                        realname={this.props.selectedUser.realname._content}
+                        location={this.props.selectedUser.location._content}
+                        description={this.props.selectedUser.description._content} />
+                </div>
+            </div>);
+        }
+    }
     getContent() {
         if(this.props.fetching) {
             return (<div className="loading">Loading...</div>);
@@ -137,6 +223,7 @@ class App extends Component {
 
         return (
             <div className="app">
+                {this.getSelectedUserPopup()}
                 {this.getContent()}
             </div>
         );
@@ -145,8 +232,9 @@ class App extends Component {
 
 const appMapStateToProps = state => {
     return {
-        fetching: state.fetching,
-        error: state.error
+        fetching: state.photos.fetching,
+        error: state.photos.error,
+        selectedUser: state.selectedUser.user
     }
 }
 
